9dc4ebdc296e66780a73930c251588999a18daed
Tingyan Xiang <tx443@login-1-1.local>
2018-05-08 12:18:11 -0400

    preliminary try with Hadoop instead of spark

---
+++ MapReduce/weather_reduce.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+#The map function for the weather data
+import sys
+import string
+
+currentkey = None
+# input comes from STDIN (stream data that goes to the program)
+for line in sys.stdin:
+	#Remove leading and trailing whitespace
+	line = line.strip()
+	#Get key/value 
+	key, value = line.split('\t',1)
+
+	#Parse key/value input
+	value = value.split(',')
+	if key == currentkey:
+		for i in range(7):
+            if value[i] != 'null': #skip null
+				counts[i] += 1
+				values[i] += float(value[i])
+	else:
+		if currentkey:
+			output = ['null']*7
+			for i in range(7):
+				if counts[i] != 0:
+                    output[i] = '{0:.4f}'.format(values[i]/counts[i]) #compute mean
+			output = ','.join(output)
+			print('{0}\t{1}'.format(currentkey, output))
+			#print('{0}\t{1}'.format(currentkey, ','.join(output)))
+
+		currentkey = key
+		values = [0,0,0,0,0,0,0]
+		counts = [0,0,0,0,0,0,0]
+
+output = ['null']*7
+for i in range(7):
+	if counts[i] != 0:
+		output[i] = '{0:.4f}'.format(values[i]/counts[i])
+output = ','.join(output)
+print('{0}\t{1}'.format(currentkey, output))
+#print('{0}\t{1}'.format(currentkey, ','.join(output)))
